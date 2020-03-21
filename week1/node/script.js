b00a5597c547c78c5c44eaca992ee523d1e3c33f
Gideron <nico.alakonni@gmail.com>
2020-03-22 01:13:22 +0200

    Node.js exercises

---
+++ week1/node/script.js
@@ -0,0 +1,9 @@
+//nodemon script.js
+console.log("This is JavaScript with nodemon!");
+
+//lodash
+var _ = require('lodash');
+console.log(_.camelCase('Foo Bar'));
+
+//express
+const express = require('express');
